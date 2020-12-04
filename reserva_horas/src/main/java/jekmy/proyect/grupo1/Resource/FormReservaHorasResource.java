75d8473479556ae8a1dbc015aaed45499cfcdff5
Edgamer63 <edgamer63@gmail.com>
2020-12-04 15:56:01 -0300

    Actualización

--- reserva_horas/src/main/java/jekmy/proyect/grupo1/Resource/FormReservaHorasResource.java
+++ reserva_horas/src/main/java/jekmy/proyect/grupo1/Resource/FormReservaHorasResource.java
@@ -46,9 +46,46 @@ public class FormReservaHorasResource {
         emailBody.setEmail(Email);
         emailBody.setSubject("¡Bienvenid@ al servicio de Reserva de Horas de JEKMY!");
 
-        String emailcontent_="Hola, te damos la bienvenida, este correo es solo un recordatorio de que te has registrado exitosamente a nuestro servicio.";
+        String emailcontent_="<body>\n" +
+                "    <table style=\"max-width: 600px; padding: 10px; margin: 0 auto; border-collapse: collapse;\">\n" +
+                "\n" +
+                "        <tr>\n" +
+                "            <td style=\"padding: 0\">\n" +
+                "\n" +
+                "            </td>\n" +
+                "        </tr>\n" +
+                "        <tr>\n" +
+                "            <td style=\"background-color: #ecf0f1\">\n" +
+                "                <div style=\"color: cornflowerblue; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif\">\n" +
+                "                    <h2 style=\"color: blue; margin: 0 0 7px\">¡Bienvenid@ a JEKMY!</h2>\n" +
+                "                    <p style=\"margin: 2px; font-size: 15px\">\n" +
+                "                        Hola, te damos la bienvenida, este correo es solo un recordatorio de que te has registrado\n" +
+                "                        exitosamente en nuestra plataforma. <br>\n" +
+                "                        ¡Enhorabuena! <br>\n" +
+                "                        Nuestra plataforma cuenta con un sistema de reserva de horas para una mejor atención y cuidado\n" +
+                "                        de nuestros clientes. <br>\n" +
+                "                        Contamos con los siguientes servicios para usted:\n" +
+                "                    </p>\n" +
+                "                    <ul style=\"font-size: 15px;  margin: 10px 0\">\n" +
+                "                        <li>Reparación, recuperación de datos y cambio de discos duros.</li>\n" +
+                "                        <li>Instalación de programas y formateo de equipos.</li>\n" +
+                "                        <li>Armado de equipos y cotizaciones.</li>\n" +
+                "                        <li>Inclusión de nuevos elementos de hardware.</li>\n" +
+                "                        <li>¡Entre muchos otros servicios informáticos!.</li>\n" +
+                "                    </ul>\n" +
+                "                    <div style=\"width: 100%;margin:20px 0; display: inline-block;text-align: center\">\n" +
+
+                "                    </div>\n" +
+                "                 \n" +
+                "                    <p style=\"color: #b3b3b3; font-size: 12px; text-align: center;margin: 30px 0 0\">JEKMY 2020</p>\n" +
+                "                </div>\n" +
+                "            </td>\n" +
+                "        </tr>\n" +
+                "\n" +
+                "\n" +
+                "    </table>";
         emailcontent_+="\n";
-        emailcontent_+="¡Enhorabuena! .";
+
 
         emailBody.setContent(emailcontent_);
         emailPort.sendEmail(emailBody);
